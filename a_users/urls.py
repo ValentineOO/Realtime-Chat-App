f46c9b4fdec389fbe5750c85e3c76168c994ac38
Valentine OO <valoladimeji@gmail.com>
2025-07-05 02:37:04 +0100

    feat(users): add profile edit view, form, and template

--- a_users/urls.py
+++ a_users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from a_users.views import profile_view
+from a_users.views import profile_view, profile_edit_view
 
 urlpatterns = [
-    path('', profile_view, name="profile")
+    path('', profile_view, name="profile"),
+    path('edit/', profile_edit_view, name="profile-edit")
 ]
--- a_users/views.py
+++ a_users/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm
 
 # Create your views here.
 
@@ -6,3 +8,9 @@ from django.shortcuts import render
 def profile_view(request):
     profile = request.user.profile
     return render(request, 'a_users/profile.html', {'profile': profile})
+
+
+@login_required
+def profile_edit_view(request):
+    form = ProfileForm(instance=request.user.profile)
+    return render(request, 'a_users/profile_edit.html', {'form': form})
